b353998182bec32813be8d31c9c85f9ae06e001b
hanherb <dyo.pyon@gmail.com>
2019-05-17 19:03:27 +0700

    Add files via upload

--- vue.config.js
+++ vue.config.js
@@ -5,12 +5,12 @@ module.exports = {
 	devServer: {
 		// host: 'localhost',
 		// host: '141.136.47.202'
-		// host: 'qrcode.wearesqood.com',
-		// https: {
-			// key: fs.readFileSync('/etc/letsencrypt/live/qrcode.wearesqood.com/privkey.pem'),
-	      	// cert: fs.readFileSync('/etc/letsencrypt/live/qrcode.wearesqood.com/fullchain.pem'),
+		host: 'qrcode.wearesqood.com',
+		https: {
+			key: fs.readFileSync('/etc/letsencrypt/live/qrcode.wearesqood.com/privkey.pem'),
+	      	cert: fs.readFileSync('/etc/letsencrypt/live/qrcode.wearesqood.com/fullchain.pem'),
   			// key: fs.readFileSync('privkey.pem'),
-	      	// cert: fs.readFileSync('fullchain.pem'),
-		// }
+	    //   	cert: fs.readFileSync('fullchain.pem'),
+		}
 	}
 }
